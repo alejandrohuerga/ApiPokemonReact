5eb54455e7da0a3d468a33268e6372a6d9f7369b
alejandrohuerga <alejandrohuerga.dev@gmail.com>
2026-03-25 13:04:07 +0100

    feat: :zap: Petición y muestreo de un solo Pokemon
    Se ha pedido y mostrado un solo Pokemon

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PokemonList from './components/PokemonList';
+
+function App() {
+  return (
+    <>
+        <h2>Pokemon API</h2>
+        <PokemonList/>
+    </>
+  )
+}
+
+export default App
